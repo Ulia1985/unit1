07e4ae60ffde51262d43299fa40351c1a8f3203e
Ulia <cs-gorodok@yandex.ru>
2023-12-09 23:30:04 +0400

    gradle.yml

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,33 @@
+package ru.netology.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+class CashbackHackServiceTest {
+    @org.junit.Test
+    void testRemain1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.Test
+    void testRemainMore1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1400;
+        int actual = service.remain(amount);
+        int expected = 600;
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.Test
+    void testRemainBelow1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 500;
+        int actual = service.remain(amount);
+        int expected = 500;
+        assertEquals(expected, actual);
+    }
+}
